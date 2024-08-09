68572b96e2c88ad34c4e9177e9cc93fb2235f064
oabdelnour <olivier.abdelnour@gmail.com>
2024-08-09 21:16:35 +0200

    feat: init

--- src/components/allTipsComponent/SpeakerForm.tsx
+++ src/components/allTipsComponent/SpeakerForm.tsx
@@ -24,23 +24,23 @@ export function SpeakerForm() {
             <div className="p-6 flex flex-col gap-2">
                 <div className="flex items-center gap-1 w-full justify-between">
                     <div className="whitespace-nowrap">Prénom :</div>
-                    <Input className="w-64" onChange={(e) => setFirstName(e.target.value)}/>
+                    <Input className="w-64" onChange={(e) => setFirstName(e.target.value)} />
                 </div>
                 <div className="flex items-center gap-1 w-full justify-between">
                     <div className="whitespace-nowrap">Nom :</div>
-                    <Input className="w-64" onChange={(e) => setLastName(e.target.value)}/>
+                    <Input className="w-64" onChange={(e) => setLastName(e.target.value)} />
                 </div>
                 <div className="overflow-ellipsis overflow-hidden text-nowrap">Nom complet : {fullName}</div>
                 <div className="flex flex-col gap-1">
                     <div>Commentaire :</div>
-                    <Textarea onChange={(e) => setComment(e.target.value)} value={comment}/>
+                    <Textarea onChange={(e) => setComment(e.target.value)} value={comment} />
                 </div>
                 <div className="flex items-center gap-1 w-full justify-between">
                     <div className="whitespace-nowrap">Age : {age}</div>
-                    <ColoredComponent/>
+                    <ColoredComponent />
                     <Button onClick={() => setAge(age + 1)}>+ 1 an</Button>
                 </div>
-                <ColoredComponent/>
+                <ColoredComponent />
             </div>
         </div>
     );
